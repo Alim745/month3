706888565f613fabeeed6cd4fa4a00244468f48a
Alim Maratov <alimmaratov11@gmail.com>
2025-08-19 18:26:27 +0600

    hw: HomeWork were added

--- main.py
+++ main.py
@@ -2,69 +2,62 @@ import flet as ft
 import datetime as dt
 
 def page_main(page: ft.Page):
-    page.title = "Мое первое приложение на flet!"
-    page.theme_mode = ft.ThemeMode.SYSTEM
+    page.title = "Мое первое приложение"
+    page.theme_mode = ft.ThemeMode.LIGHT
 
     greeting_history = []
 
     greeting_text = ft.Text("Hello World")
     name_input = ft.TextField(label="Введите имя: ")
 
-    def get_greeting(name):
-        hour = dt.datetime.now().hour
-        if 6 <= hour < 12:
-            return f"Доброе утро, {name}!"
-        elif 12 <= hour < 18:
-            return f"Добрый день, {name}!"
-        elif 18 <= hour < 24:
-            return f"Добрый вечер, {name}!"
-        else:
-            return f"Доброй ночи, {name}!"
-
     def update_history_view():
-        history_controls = [ft.Text("История приветствий: ")]
-        for idx, name in enumerate(greeting_history):
-            history_controls.append(
-                ft.Row([ft.Text(name), 
-                        ft.IconButton(icon=ft.Icons.CLOSE, on_click=lambda e, i=idx: remove_name_from_history[i])
-                        ])
-            )
+        history_controls = [ft.Text("История приветствий:")]
+        for entry in greeting_history:
+            history_controls.append(ft.Text(entry))
         history_column.controls = history_controls
-        page.update
+        page.update()
 
-    def remove_name_from_history(index):
-        if 0 <= index < len(greeting_history):
-            del greeting_history[index]
-            update_history_view()
+    def clear_history(_):
+        greeting_history.clear()
+        update_history_view()
 
-    def on_button_click():
+    clear_row = ft.Row(
+        controls=[ft.Text("Очистить историю"), ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history),],
+        alignment=ft.MainAxisAlignment.CENTER)
+
+    def on_button_click(_):
         name = name_input.value.strip()
-        print(name)
 
         if name:
-            greeting_text.value = f"Hello {name}"
+            current_hour = dt.datetime.now().hour
+            if 6 <= current_hour < 12:
+                greeting = "Доброе утро"
+            elif 12 <= current_hour < 18:
+                greeting = "Добрый день"
+            elif 18 <= current_hour < 24:
+                greeting = "Добрый вечер"
+            else:
+                greeting = "Доброй ночи"
+
+            greeting_text.value = f"{greeting}, {name}!"
             name_input.value = ""
             greet_button.text = "Поздороваться снова"
 
-            greeting_history.append
+            greeting_history.append(f"{dt.datetime.now()}: {name}")
             update_history_view()
         else:
             greeting_text.value = "Введите корректное имя!"
 
-        page.update
+        page.update()
 
-    greet_button = ft.ElevatedButton("Отправить", on_click=on_button_click)
-    greet_button_icon = ft.IconButton(icon=ft.Icons.SEND, on_click=on_button_click, icon_color=ft.Colors.GREEN)
-
+    greet_button = ft.ElevatedButton("Отправить", on_click=on_button_click, icon=ft.Icons.SEND)
+    clear_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
+    greet_button = ft.ElevatedButton("Поздороваться снова", on_click=on_button_click)
 
     history_column = ft.Column([])
     update_history_view()
 
 
-    # page.add(greeting_text, name_input, greet_button, history_column)
-
-
-    page.add(ft.Row([greeting_text, name_input, greet_button_icon], alignment=ft.MainAxisAlignment.CENTER), history_column)
-
+    page.add(clear_row, greeting_text, name_input, greet_button, history_column)
 
 ft.app(target=page_main)
